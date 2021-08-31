dd89e5822389fbd83df7ff82e8518f25929f3d5c
Badgey <andrewbadge@outlook.com>
2021-08-31 21:10:51 +1000

    Detect if Client is already running

--- DNS Roaming Client/Program.cs
+++ DNS Roaming Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DNS_Roaming_Common;
 using System;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace DNS_Roaming_Client
@@ -13,17 +14,31 @@ namespace DNS_Roaming_Client
         [STAThread]
         static void Main()
         {
-            try { 
+            Mutex mutex = new System.Threading.Mutex(false, "DNSRoamingClientMutex");
+            try {
                 Logger.Info("--------------------------");
                 Logger.Info(String.Format("Starting ({0})", Assembly.GetExecutingAssembly().GetName().Version.ToString()));
 
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new FrmMain());
+                if (mutex.WaitOne(0, false))
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new FrmMain());
+                }
+                else
+                    Logger.Info("Exiting as another instance is already running");
             }
             catch (Exception ex)
             {
                 Logger.Error(ex.Message);
+            }
+            finally
+            {
+                if (mutex != null)
+                {
+                    mutex.Close();
+                    mutex = null;
+                }
             }
         }
     }
